35a0757623800651ac80a7d9ef7bfb2fab7f872a
rareddy2 <rareddy2@illinois.edu>
2022-11-05 16:59:24 -0700

    Adding grouping algorithm

--- extract_blocks.py
+++ extract_blocks.py
@@ -121,4 +121,79 @@ def label(im):
 im = preprocess('data/leonardo.jpg')
 labeled_im = label(im)
 
-#push test comment
+############ Calculating centre of mass and orientation and grouping ############
+def calculate_mu(x_coords, y_coords, x_bar, y_bar, p, q):
+    mu = 0
+    for i in range(len(x_coords)):
+        for j in range(len(y_coords)):
+            mu += np.power((x_coords[i] - x_bar),p) * np.power((y_coords[j] - y_bar),q)
+
+    return mu
+
+def get_component_indices(labeled_im, cur_label):
+    block_number_coords = np.zeros(cur_label)
+    x_bar_all = np.zeros(cur_label)
+    y_bar_all = np.zeros(cur_label)
+    theta_all = np.zeros(cur_label)
+    for cur_label in range(1, cur_label + 1):
+        x_coords = np.where( labeled_im == cur_label )[0] )
+        y_coords = np.where( labeled_im == cur_label )[1] )
+        block_number_coords = len(x_coords)
+
+        x_bar[cur_label - 1] = np.int( np.average(x_coords) )
+        y_bar[cur_label - 1] = np.int( np.average(y_coords) )
+
+        mu_1_1 = calculate_mu(x_coords, y_coords, x_bar[cur_label - 1], y_bar[cur_label - 1], 1, 1)
+        mu_2_0 = calculate_mu(x_coords, y_coords, x_bar[cur_label - 1], y_bar[cur_label - 1], 2, 0)
+        mu_0_2 = calculate_mu(x_coords, y_coords, x_bar[cur_label - 1], y_bar[cur_label - 1], 0, 2)
+        
+        theta[cur_label - 1] = 0.5 * ( np.arctan2( ( 2*mu_1_1 ) / (mu_2_0 - mu_0_2) ) )
+
+    return x_bar_all, y_bar_all, theta_all, block_number_coords
+
+def find_blocks_within_radius(cur_block_idx, labeled_im, x_bar, y_bar, radius):
+    min_y = np.floor(y_bar - r)
+    max_y = np.ceil(y_bar + r)
+
+    blocks_within_radius = []
+    for y in range(min_y + 1, max_y):
+        # From the formula: r^2 = (x-h)^2 + (y-k)^2, where 
+        # h,k are the distances from x,y respectively
+        x_diff = np.power(radius,2) - np.power(y-y_bar,2)
+        min_x = np.floor(x_bar - x_diff)
+        max_x = np.ceil(x_bar + x_diff)
+
+        for x in range(min_x + 1, max_x):
+            if labeled_im[x,y] != 0 and labeled_im[x,y] != cur_block_idx:
+                if labeled_im[x,y] not in blocks_within_radius:
+                    blocks_within_radius.append(labeled_im[x,y])
+
+
+def find_nearest_block(cur_block_idx, blocks_within_radius, x_bar, y_bar):
+    min_distance_block = None
+    min_distance = np.inf
+    for block in blocks_within_radius:
+        x_dist = np.power(x_bar[block - 1] - x_bar[cur_block_idx], 2)
+        y_dist = np.power(y_bar[block - 1] - y_bar[cur_block_idx], 2)
+        distance_to_cur_block = np.sqrt(x_dist + y_dist)
+        if distance_to_cur_block < min_distance:
+            min_distance = distance_to_cur_block
+            min_distance_block = block
+
+    return min_distance_block 
+
+
+def grouping(labeled_im, cur_label, block_number_coords, x_bar, y_bar):
+    N_max = 30 #??????? This changes based on the image - need to test different values
+    for N in range(1, N_max + 1):
+        radius = 8 - ( 6 * ( (N - 1) / (N_max -1) ) )
+        
+        for i in range(cur_label):
+            blocks_within_radius = []
+            if block_number_coords[i] == N:
+                blocks_within_radius = find_blocks_within_radius(i, labeled_im, x_bar[i], y_bar[i], radius)
+            
+            if len(blocks_within_radius) > 0:
+                nearest_block = find_nearest_block(i, blocks_within_radius, x_bar, y_bar)
+
+            #if block_number_coords[i] < N_max #### Merge step (based on what the matching algorithm needs)
